6fc9efc0ace5348bea6e39015f079988bd08dde7
ddiu8081 <ddiu8081@gmail.com>
2022-10-31 00:54:32 +0800

    chore: add base palette

---
+++ themes/palette.ts
@@ -0,0 +1,28 @@
+export const palette = {
+  primary: '#ff8787',
+  secondary: '#66b395',
+  foreground: '#999999',
+  disabled: '#444444',
+  border: '#ffffff1a',
+
+  token: {
+    default: '#000000',
+    variable: '#000000',
+    keyword: '#000000',
+    number: '#000000',
+    constant: '#000000',
+    function: '#000000',
+    string: '#000000',
+    operator: '#000000',
+    comment: '#000000',
+  },
+
+  black: '#000000',
+  red: '#ff8787',
+  green: '#aaff87',
+  yellow: '#ffff87',
+  blue: '#87aaff',
+  magenta: '#ff87ff',
+  cyan: '#87ffff',
+  white: '#ffffff',
+}
